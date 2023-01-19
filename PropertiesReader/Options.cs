629a5d3ddc945a44eb65d0f0cd1983a321b527eb
jevgenijs <banderge"hotmail.com>
2023-01-19 07:07:35 +0200

    initial

---
+++ PropertiesReader/Options.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+
+public class Options
+{
+    [Option('b', "bindDir", Required = true, HelpText = "Binary Directory")]
+    public string BinaryDirectory { get; set; }
+
+    [Option('t', "text", Required = true, HelpText = "Text (selected class name)")]
+    public string ClassName { get; set; }
+
+    [Option('p', "projectName", Required = true, HelpText = "csproj name,")]
+    public string ProjectName { get; set; }
+}
---
+++ PropertiesReader/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using System.Reflection;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(o =>
+    {
+        Console.WriteLine($"Binary Path: {o.BinaryDirectory}");
+        Console.WriteLine($"ProjectName: {o.ProjectName}");
+        Console.WriteLine($"Class/Struct Name: {o.ClassName}");
+
+        var projectName = o.ProjectName.Trim();
+        if (projectName.EndsWith(".csproj"))
+        {
+            projectName = projectName.Substring(0, projectName.Length - ".csproj".Length);
+            projectName += ".dll";
+        }
+
+        var asm = Assembly.LoadFrom(o.BinaryDirectory + projectName);
+        var type = asm.GetType(o.ClassName);
+
+        foreach (var prop in type.GetProperties())
+        {
+            Console.WriteLine(prop.Name);
+        }
+    });
